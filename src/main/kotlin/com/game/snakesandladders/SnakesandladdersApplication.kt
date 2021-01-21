41fb04ddbd75f65d78f3a036d43a2ffe03542b15
Nataliia Grynko <nataliia_grynko@epam.com>
2021-01-21 16:54:21 +0200

    Initial commit. generated Spring project.

---
+++ src/main/kotlin/com/game/snakesandladders/SnakesandladdersApplication.kt
@@ -0,0 +1,11 @@
+package com.game.snakesandladders
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SnakesandladdersApplication
+
+fun main(args: Array<String>) {
+	runApplication<SnakesandladdersApplication>(*args)
+}
---
+++ src/test/kotlin/com/game/snakesandladders/SnakesandladdersApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.game.snakesandladders
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SnakesandladdersApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
