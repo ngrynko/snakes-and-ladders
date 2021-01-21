2dc3906284e53a918d59c6ab4999c995fa004fc7
Nataliia Grynko <nataliia_grynko@epam.com>
2021-01-21 17:32:31 +0200

    Add implementation for if player has win.

--- src/main/kotlin/com/game/snakesandladders/Player.kt
+++ src/main/kotlin/com/game/snakesandladders/Player.kt
@@ -1,8 +1,12 @@
 package com.game.snakesandladders
 
 data class Player(var currentPosition: Int = 1) {
-    
+
     fun rollDie(): Int{
         return (1..6).random()
+    }
+
+    fun won(): Boolean{
+        return currentPosition == 100
     }
 }
